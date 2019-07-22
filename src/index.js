20c600c08d069de7656dc4c8c978faababcdca06
Oliver Sturm <oliver@oliversturm.com>
2019-07-22 16:13:53 +0100

    adding URLSearchParams polyfill for IE and Edge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'url-search-params-polyfill';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
